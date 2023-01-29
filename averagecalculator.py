2f7a56eac1e08f79576514190bd978a1422725db
Nathan Knight <natelyleknight@gmail.com>
2023-01-28 19:47:18 -0500

    Update 1.1.0
    - Made Errors more readable
    - Added fallback for when no numbers where in the list and you tried to delete
    - Added wait times to make everything more fluid
    - Added secret message  >:3

--- averagecalculator.py
+++ averagecalculator.py
@@ -1,12 +1,16 @@
+import time
+
 numberslist = []
 
+
 def FirstCalculations():
-    print("To calculate an average of a set of numbers, please enter the first number below")
+    print("Please enter the first number of the list you would like to average")
     fnum = input()
     try:       
         fnum = int(fnum)
         numberslist.append(fnum)
         print("Added: " + str(fnum))
+        time.sleep(0.5)
         ExtendedCalculations()
 
     except ValueError:
@@ -14,34 +18,43 @@ def FirstCalculations():
             fnum = float(fnum)
             numberslist.append(fnum)
             print("Added: " + str(fnum))
+            time.sleep(0.5)
             ExtendedCalculations()
         except ValueError:
-            print("Im sorry, you cannot input a string, try again!")
+            print("Invalid Syntax | Please enter a number!")
+            time.sleep(0.5)
             FirstCalculations()
             
     fnum = int(fnum)
     numberslist.append(fnum)
     print("Added: " + str(fnum))
+    time.sleep(0.5)
     ExtendedCalculations()
 
 def ExtendedCalculations():
-    print("Please enter the next number! | If you're done with your list (" + str(numberslist) + ") enter 'done' | If you would like to delete the last number in the list, enter 'delete'")
+    print("Please enter the next number \n If you're done with your list " + str(numberslist) + " enter 'done' \n If you would like to delete the last number in the list, enter 'delete'")
     num = input()
     if num == str("done"):
         numberlength = len(numberslist)
         numbertotal = sum(numberslist)
         average = numbertotal / numberlength
-        print('Okay, heres your average: ' + str(average))
+        print('Average: ' + str(average))
         exit()
     if num == str("delete"):
+        if numberslist ==  []:
+            print("There is no numbers in the list")
+            time.sleep(0.5)
+            ExtendedCalculations()
         del numberslist[-1]
-        print("Deleted the last number in the list!")
+        print("Deleted the last number in the list")
+        time.sleep(0.5)
         ExtendedCalculations()
     
     try:       
         num = int(num)
         numberslist.append(num)
         print("Added: " + str(num))
+        time.sleep(0.5)    
         ExtendedCalculations()
 
     except ValueError:
@@ -49,9 +62,13 @@ def ExtendedCalculations():
             num = float(num)
             numberslist.append(num)
             print("Added: " + str(num))
+            time.sleep(0.5)
             ExtendedCalculations()
         except ValueError:
-            print("Im sorry, that is not a valid option, please try again!")
+            print("Invalid Syntax | Please enter a number!")
+            time.sleep(0.5)
             ExtendedCalculations()
 
 FirstCalculations()
+
+# If you see this, ur gay :3
